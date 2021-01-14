3626c7be81080c85203f31d29577345ded7c9968
Addison Reynolds <areynolds24@biomedscienceacademy.org>
2021-01-14 23:05:08 +0000

    I added a random module and asked the user their name.

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ import importlib.util
 import random
 
 def run_tests():
-  """
-  This function will check for the pytest module
-  before calling it to run the included tests.py
-  """
-  if importlib.util.find_spec('pytest') is None: # Check if pytest is installed
-    os.system('python3 -m pip install -q pytest')
-
-  command = "python3 -m pytest --tb=line -v tests.py"
+  """This function will check for the pytest module"""
+  command = "python3 -m pytest --tb=line -v -s tests.py"
   print(command)
   os.system(command)
 
 def main():
   """This function contains all code for the chatbot."""
   print("Hello!")
-
+  print("My name is Chat Bot and my friends call me Fiona!")
+  name = input("What's your name?: ")
+  import random
+  name_list = ["That's Awesome", "How cool", "I love your name", name + "Is a great name!"]
+  print(random.choice(name_list))
+  name
 
 if __name__ == "__main__":
   main()
