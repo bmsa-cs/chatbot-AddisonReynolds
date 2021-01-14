41bbc79fabe597dc14fddb55b72af9b3cbc9c985
Addison Reynolds <areynolds24@biomedscienceacademy.org>
2021-01-14 23:53:58 +0000

    I added more to the main function. I added an if statement and more questions that the user will be asked.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Period/Core:
 
 import os
 import importlib.util
-
 import random
 
 def run_tests():
@@ -21,10 +20,27 @@ def main():
   print("Hello!")
   print("My name is Chat Bot and my friends call me Fiona!")
   name = input("What's your name?: ")
-  import random
-  name_list = ["That's Awesome", "How cool", "I love your name", name + "Is a great name!"]
+  name_list = ["That's Awesome", "How cool", "I love your name", name + " is a great name!"]
   print(random.choice(name_list))
-  name
+  wyd = input("What are you doing?:")
+  wyd
+  wyd_list = ["Oh ok", "cool", "Nice"]
+  print(random.choice(wyd_list))
+  color = input("What's your favorite color?")
+  color
+  color_list = ["that's awesome", "Mine too", "Oh wow", "cool" ]
+  print(random.choice(color_list))
+  grade = int(input("What grade are you in?(1-12):"))
+  grade 
+  if  grade <= 5: 
+    print("You're in Elementary School. That's Awesome!")
+  elif grade > 5 and grade <= 8:
+    print("You're in Middle School. That's Awesome!")
+  elif grade > 8 and grade <= 9: 
+    print("You're in High School. That's Awesome!")
+  else:
+    print("You're not in school. That's Awesome!")
+    
 
 if __name__ == "__main__":
   main()
